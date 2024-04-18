be003f3b5fa44b783bc975a14882eac8d8461a82
markang812 <markang812@gmail.com>
2024-04-18 09:47:26 +0800

    new components

---
+++ app/ui/dashboard/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+"use client"
+import { usePathname } from "next/navigation"
+import styles from "./navbar.module.css"
+
+const Navbar = () => {
+    const pathname = usePathname
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+}
+
+export default Navbar;
