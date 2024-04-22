7912cd5dfc90f0d6bb3cb1f85fbb1e9dd158a1e8
markang812 <markang812@gmail.com>
2024-04-22 21:50:42 +0800

    adding users and products pages

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,7 +1,62 @@
+import Image from "next/image"
+import Link from "next/link"
+import styles from "@/app/ui/dashboard/products/products.module.css"
+import Search from "@/app/ui/dashboard/search/search"
+import Pagination from "@/app/ui/dashboard/pagination/pagination"
+
 const ProductsPage = () => {
     return (
-        <div>
-            ProductsPage
+        // <div>Products page</div>
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <Search placeholder="Search for a product"/>
+                <Link href="/dashboard/products/add" >
+                    <button className={styles.addButton}>Add New Stand</button>
+                </Link>
+            </div>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td>Title</td>
+                        <td>Description</td>
+                        <td>Price</td>
+                        <td>Created at</td>
+                        <td>Stock</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className={styles.product}>
+                                <Image 
+                                    src="/noproduct.jpg"
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.productImage}
+                                />
+                                iPhone
+                            </div>
+                        </td>
+                        <td>Apple iPhone 15 Pro Max</td>
+                        <td>$69</td>
+                        <td>13-04-24</td>
+                        <td>69</td>
+                        <td>
+                            <div className={styles.buttons}>
+                                <Link href="/">
+                                    <button className={`${styles.button} ${styles.view}`}>View</button>
+                                </Link>
+                                <Link href="/">
+                                    <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                                </Link>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <Pagination />
         </div>
     );
 }
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,7 +1,60 @@
+import Pagination from "@/app/ui/dashboard/pagination/pagination"
+import Search from "@/app/ui/dashboard/search/search"
+import styles from "@/app/ui/dashboard/users/users.module.css"
+import Image from "next/image"
+import Link from "next/link"
 const UsersPage = () => {
     return (
-        <div>
-            UsersPage
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <Search placeholder="Enter a Stand Name"/>
+                <Link href="/dashboard/users/add" >
+                    <button className={styles.addButton}>Add New Stand</button>
+                </Link>
+            </div>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Created at</td>
+                        <td>Role</td>
+                        <td>Status</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image 
+                                    src="/noavatar.png"
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.userImage}
+                                />
+                                Jonathan Joestar
+                            </div>
+                        </td>
+                        <td>jj@stardustcrusaders.com</td>
+                        <td>13-04-24</td>
+                        <td>Admin</td>
+                        <td>active</td>
+                        <td>
+                            <div className={styles.buttons}>
+                                <Link href="/">
+                                    <button className={`${styles.button} ${styles.view}`}>View</button>
+                                </Link>
+                                <Link href="/">
+                                    <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                                </Link>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <Pagination />
         </div>
     );
 }
