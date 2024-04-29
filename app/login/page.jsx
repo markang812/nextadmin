3ebcfe37f667b711124a5a6e8ee06005384f19f2
Mark <=>
2024-04-29 22:44:00 +0800

    new login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,16 @@
-const LoginPage = () => {
-    return (
-        <div>
-            LoginPage
+import styles from "@/app/ui/login/login.module.css"
+
+const Login = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.form}>
+            <h1>Login</h1>
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+            <button type="submit">Login</button>
         </div>
-    );
+    </div>
+  )
 }
 
-export default LoginPage;
+export default Login
